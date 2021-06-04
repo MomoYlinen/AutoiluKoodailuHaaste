bf69da1546298f4cbb50cd75abd71713b8d72065
InpossibleMomo <momo.ylinen@outlook.com>
2021-06-04 22:01:08 +0300

    final versio 1.0

--- src/components/form.js
+++ src/components/form.js
@@ -1,29 +1,28 @@
+import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import FilledInput from '@material-ui/core/FilledInput';
 import {Speed,Directions,} from '@material-ui/icons/'
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
-import miniVan from '../cars/minivan-car.svg'
-import sedanCar from '../cars/sedan-car-model.svg'
-import cityCar from '../cars/car-city-model.svg'
-import divStyles from '../styles/stylesDiv'
+import miniVan from '../styles/cars/minivan-car.svg'
+import sedanCar from '../styles/cars/sedan-car-model.svg'
+import cityCar from '../styles/cars/car-city-model.svg'
+import formStyles from '../styles/formStyles'
 import useStyles from '../styles/useStyles'
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import '../App.css'
-
 
 const Form = (props) => {
 
     const classes = useStyles()
 
     return (
-<>
+<div>
   <Grid container className={classes.grid} spacing={6}>
-    <Paper className={classes.paper}>
+    <Paper className={classes.paper} elevation={3} variant="outlined">
       <div className='backround'>
         <Grid item xs={12}>
           <Grid container justify="center" className={classes.buttonGroupA.buttonContainer}>
@@ -34,13 +33,13 @@ const Form = (props) => {
               aria-label="text-aligment"
               className={classes.buttonGroupA.buttonGroup}>
               <ToggleButton value="Car1" aria-label="left aligned" onClick={props.fuelchange1} className={classes.buttonGroupA.toggleButtons}>
-                <label style={divStyles.buttonGroup.label1}> <img src={cityCar} style={divStyles.buttonGroup.labelImage} alt={cityCar}/><p style={divStyles.buttonGroup.labeltext}> Auto 1 </p></label>
+                <label style={formStyles.buttonGroup.label1}> <img src={cityCar} style={formStyles.buttonGroup.labelImage} alt={cityCar}/><p style={formStyles.buttonGroup.labeltext}> Auto 1 </p></label>
               </ToggleButton>
               <ToggleButton  value="Car2"  aria-label="centered" onClick={props.fuelchange2} className={classes.buttonGroupA.toggleButtons}>
-                <label style={divStyles.buttonGroup.label1} > <img src={sedanCar} style={divStyles.buttonGroup.labelImage} alt={sedanCar}/><p style={divStyles.buttonGroup.labeltext}> Auto 2 </p></label>
+                <label style={formStyles.buttonGroup.label1} > <img src={sedanCar} style={formStyles.buttonGroup.labelImage} alt={sedanCar}/><p style={formStyles.buttonGroup.labeltext}> Auto 2 </p></label>
               </ToggleButton>
               <ToggleButton  value="Car3" aria-label="right aligned" onClick={props.fuelchange3} className={classes.buttonGroupA.toggleButtons}>
-                <label style={divStyles.buttonGroup.label1}> <img src={miniVan} style={divStyles.buttonGroup.labelImage} alt={miniVan}/><p style={divStyles.buttonGroup.labeltext}> Auto 3 </p></label>
+                <label style={formStyles.buttonGroup.label1}> <img src={miniVan} style={formStyles.buttonGroup.labelImage} alt={miniVan}/><p style={formStyles.buttonGroup.labeltext}> Auto 3 </p></label>
               </ToggleButton>
             </ToggleButtonGroup>
           </Grid>
@@ -69,8 +68,8 @@ const Form = (props) => {
                     <InputLabel htmlFor="component-filled"><Speed/></InputLabel>
                     <FilledInput id="component-filled" value={props.speed2} onChange={props.handleSpeed2} type='number'/>
                   </FormControl>
-                <div style= {divStyles.button.buttonStyle}>
-              <Button variant="contained" color="primary" type={props.type} >
+                <div style= {formStyles.button.buttonStyle}>
+              <Button variant="outlined" color="primary" type={props.type} size='large'>
                 HAE
               </Button>
                 </div>
@@ -80,9 +79,8 @@ const Form = (props) => {
       </div>
     </Paper>
   </Grid>
-</>
+</div>
     )
-
 }
 
 export default Form
---
+++ src/styles/resultsStyles.js
@@ -0,0 +1,10 @@
+const resultsStyles = {
+    results: {
+        results: { display: 'flex',flexDirection: 'column', padding: '10px 10px 10px 0px'},
+        resultsHeader: {padding: '0px 10px 30px 10px', transition:'ease' },
+        showResults: {padding: '0px 0px 20px 0px'},
+        answerStyle: {textAlign: 'center', padding:'20px'},
+        answerButton:{padding:'40px'}
+    }
+}
+export default resultsStyles
